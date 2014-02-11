a04a964eddb2c5c450144d51b81ea1fc2a64d04a
Rob Halff <rob.halff@gmail.com>
2014-02-11 17:59:16 +0100

    add password type

---
+++ nodes/password/node.js
@@ -0,0 +1,12 @@
+/* simple prototype to distinct passwords from strings
+ * Not to collect passwords, but to warn.. :)
+ * */
+function password(pass) {
+  this.pass = pass;
+};
+
+password.prototype.toString = function passwordToString() {
+  return this.pass;
+};
+
+output.out = new password(input.in);
